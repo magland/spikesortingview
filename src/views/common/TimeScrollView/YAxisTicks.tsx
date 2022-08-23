8ac859e83409b037b3dd77b48249ad3a6a6b5023
Jeff Soules <jsoules@flatironinstitute.org>
2022-08-23 17:35:16 -0400

    Commenting a confusing function.

--- src/views/common/TimeScrollView/YAxisTicks.tsx
+++ src/views/common/TimeScrollView/YAxisTicks.tsx
@@ -32,27 +32,65 @@ const range = (min: number, max: number, step: number, base: number) => {
         .filter(x => x > min)
 }
 
+/**
+ * Find a set of appropriately-scaled grid lines for the Y axis of a plot.
+ * We assume that we have data over some range, and want to break the Y axis into ticks
+ * that will generate enough grid lines to be useful, but not so many that they will make
+ * it hard to read the plot or otherwise obscure the data.
+ * 
+ * Grid lines are considered valid if each line represents a step of 1, 2, or 5 base units,
+ * where the base unit is some power of 10 of the data displayed in the chart. Conceptually,
+ * if we have measurements from (e.g.) 123 ml to 187 ml, we will first see if 1-ml grid lines work;
+ * this is probably too many, so we'll then try having each grid line/y-axis tick indicate 2 ml;
+ * then 5 ml; then 10 ml, 20 ml, etc. until we eventually find an acceptable size.
+ * 
+ * @param minGridLines Minimum number of grid lines that should be present on the plot.
+ * @param maxGridLines Maximum number of grid lines before the plot appears too busy.
+ * @param range The range of the values displayed in the plot, normalized to ensure it's got 3 digits
+ * before the decimal point.
+ * @returns {step: number, scale: number} a dictionary indicating the scale (power of 10) of the chosen
+ * grid lines and the size of each step (1, 2, or 5) at that order of magnitude. The distance between
+ * any two grid line ticks would then be step * 10 ** scale.
+ */
 const fitGridLines = (minGridLines: number, maxGridLines: number, range: number): {step: number, scale: number} => {
     let scale = 0
     while (true) {
-        const steps = [1, 2, 5]
-        const realizedScale = Math.pow(10, scale)
+        const steps = [1, 2, 5]  // candidate step sizes are 1, 2, 5, (10, 20, 50, 100, 200, ...)
+        const realizedScale = Math.pow(10, scale)   // realizedScale = the base order of magnitude represented by `scale`.
         const results: {step: number, scale: number}[] = []
-        for (let s of steps) {
+        for (let s of steps) {  // for each of the 3 candidate step sizes at this order of magnitude
+            // determine how many steps of [1 | 2 | 5] * 10^scale would be needed to exhaust the range.
+            // (technically on the plot this will be the number of spaces between the grid lines, thus the hard </>).
             const fit = range/(s * realizedScale)
             if (fit > minGridLines && fit < maxGridLines) {
+                // if this split produces an acceptable number of counts, record the step and scale sizes that work.
                 results.push({step: s, scale: scale})
             }
-            // this means the step size is too big. This shouldn't really happen without finding an acceptable step size first,
-            // but we'll check for it later just in case.
-            if (fit < minGridLines) { return {step: -1, scale: -1} }
-            results.push({step: 0, scale: 0})
+            if (fit < minGridLines) {
+                // step size was too big--it resulted in too few divisions of the range.
+                results.push({step: -1, scale: -1})
+                break
+            }
         }
+        // Completing this loop means we finished evaluating one order of magnitude. If we found
+        // a step-scale combination that gave a number of divisions in the acceptable range,
+        // it should show up as a result record with a positive step size (...viz, 1, 2, or 5).
+        // We never have to worry about it actually being from a different scale, because we run
+        // this operation every time we complete an order of magnitude.
         const a = results.find(r => r.step > 0)
         if (a) return a
+        // On the other hand, if the result set includes an entry with a step size of -1, then
+        // the last loop's step size got too big. This is bad--if we see this, it means it
+        // is not productive to try larger step sizes, but if we had found something that did
+        // work, we would've returned that first. So realistically this probably means the
+        // range of allowed grid lines is too small. (Those are set programmatically from
+        // a range of allowable inter-tick pixel counts, minGridSpacingPx and maxGridSpacingPx,
+        // which might need to be adjusted.)
         const b = results.find(r => r.step === -1)
         if (b) return b
-        scale = scale + 1
+        scale = scale + 1 // Try the next larger order of magnitude.
+        // If a step size of 50_000_000_000 base units still produces too many lines, give up.
+        // (This should never happen, since we normalized the input range to 100-1000.)
         if (scale > 10) {
             return {step: 1, scale: 0}
         }
