bd35c7c6a23150297b588241c02e78f8a92b2de9
Jeremy Magland <jmagland@flatironinstitute.org>
2022-11-10 11:23:56 -0500

    memoize view loader

--- src/View.tsx
+++ src/View.tsx
@@ -13,7 +13,7 @@ import { SortingLayoutView } from 'libraries/view-sorting-layout';
 import { SortingSelectionView } from 'libraries/view-sorting-selection';
 import { SummaryView } from 'libraries/view-summary';
 import { Test1View } from 'libraries/view-test-1';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useMemo } from 'react';
 import { isViewData } from 'ViewData';
 import {loadView as loadCoreView} from '@figurl/core-views'
 import {loadView as loadTimeseriesView} from '@figurl/timeseries-views'
@@ -34,67 +34,73 @@ type Props = {
 }
 
 const View: FunctionComponent<Props> = ({data, width, height, opts}) => {
-    const viewLoaders = [loadCoreView, loadTimeseriesView, loadSpikeSortingView, loadFranklabView]
-    for (let loadView of viewLoaders) {
-        const v = loadView({data, width, height, opts, ViewComponent: View})
-        if (v) return v
-    }
-    if (!isViewData(data)) {
-        console.warn(data)
-        throw Error('Invalid view data')
-    }
-    if (data.type === 'Composite') {
-        return <CompositeView data={data} ViewComponent={View} width={width} height={height} />
-    }
-    else if (data.type === 'MultiTimeseries') {
-        return <MultiTimeseriesView data={data} ViewComponent={View} width={width} height={height} />
-    }
-    else if (data.type === 'Summary') {
-        return <SummaryView data={data} width={width} height={height} />
-    }
-    else if (data.type === 'MountainLayout') {
-        return <MountainLayoutView data={data} ViewComponent={View} width={width} height={height} />
-    }
-    else if (data.type === 'PositionPlot') {
-        return <PositionPlotView data={data} timeseriesLayoutOpts={opts} width={width} height={height} />
-    }
-    else if (data.type === 'PositionPdfPlot') {
-        return <PositionPdfPlotView data={data} timeseriesLayoutOpts={opts} width={width} height={height} />
-    }
-    else if (data.type === 'LivePositionPdfPlot') {
-        return <LivePositionPdfPlotView data={data} timeseriesLayoutOpts={opts} width={width} height={height} />
-    }
-    else if (data.type === 'Epochs') {
-        return <EpochsView data={data} timeseriesLayoutOpts={opts} width={width} height={height} />
-    }
-    else if (data.type === 'Console') {
-        return <ConsoleView data={data} width={width} height={height} />
-    }
-    else if (data.type === 'SortingLayout') {
-        return <SortingLayoutView data={data} ViewComponent={View} width={width} height={height} />
-    }
-    else if (data.type === 'SortingCuration') {
-        return <SortingCurationView data={data} width={width} height={height} />
-    }
-    else if (data.type === 'TiledImage') {
-        return <TiledImageComponent data={data} width={width} height={height} />
-    }
-    else if (data.type === 'SortingSelection') {
-        return <SortingSelectionView data={data} width={width} height={height} />
-    }
-    else if (data.type === 'LiveEvaluateFunction') {
-        return <LiveEvaluateFunctionView data={data} width={width} height={height} />
-    }
-    else if (data.type === 'ExperimentalSelector1') {
-        return <ExperimentalSelector1View data={data} width={width} height={height} />
-    }
-    else if (data.type === 'Test1') {
-        return <Test1View data={data} width={width} height={height} />
-    }
-    else {
-        console.warn('Unsupported view data', data)
-        return <div>Unsupported view data: {data['type']}</div>
-    }
+    // It's important to memoize this
+    // because validation of data can be slow
+    const v = useMemo(() => {
+        const viewLoaders = [loadCoreView, loadTimeseriesView, loadSpikeSortingView, loadFranklabView]
+        for (let loadView of viewLoaders) {
+            const v = loadView({data, width, height, opts, ViewComponent: View})
+            if (v) return v
+        }
+        if (!isViewData(data)) {
+            console.warn(data)
+            throw Error('Invalid view data')
+        }
+        if (data.type === 'Composite') {
+            return <CompositeView data={data} ViewComponent={View} width={width} height={height} />
+        }
+        else if (data.type === 'MultiTimeseries') {
+            return <MultiTimeseriesView data={data} ViewComponent={View} width={width} height={height} />
+        }
+        else if (data.type === 'Summary') {
+            return <SummaryView data={data} width={width} height={height} />
+        }
+        else if (data.type === 'MountainLayout') {
+            return <MountainLayoutView data={data} ViewComponent={View} width={width} height={height} />
+        }
+        else if (data.type === 'PositionPlot') {
+            return <PositionPlotView data={data} timeseriesLayoutOpts={opts} width={width} height={height} />
+        }
+        else if (data.type === 'PositionPdfPlot') {
+            return <PositionPdfPlotView data={data} timeseriesLayoutOpts={opts} width={width} height={height} />
+        }
+        else if (data.type === 'LivePositionPdfPlot') {
+            return <LivePositionPdfPlotView data={data} timeseriesLayoutOpts={opts} width={width} height={height} />
+        }
+        else if (data.type === 'Epochs') {
+            return <EpochsView data={data} timeseriesLayoutOpts={opts} width={width} height={height} />
+        }
+        else if (data.type === 'Console') {
+            return <ConsoleView data={data} width={width} height={height} />
+        }
+        else if (data.type === 'SortingLayout') {
+            return <SortingLayoutView data={data} ViewComponent={View} width={width} height={height} />
+        }
+        else if (data.type === 'SortingCuration') {
+            return <SortingCurationView data={data} width={width} height={height} />
+        }
+        else if (data.type === 'TiledImage') {
+            return <TiledImageComponent data={data} width={width} height={height} />
+        }
+        else if (data.type === 'SortingSelection') {
+            return <SortingSelectionView data={data} width={width} height={height} />
+        }
+        else if (data.type === 'LiveEvaluateFunction') {
+            return <LiveEvaluateFunctionView data={data} width={width} height={height} />
+        }
+        else if (data.type === 'ExperimentalSelector1') {
+            return <ExperimentalSelector1View data={data} width={width} height={height} />
+        }
+        else if (data.type === 'Test1') {
+            return <Test1View data={data} width={width} height={height} />
+        }
+        else return undefined
+    }, [data, height, width, opts])
+    
+    if (v) return v
+
+    console.warn('Unsupported view data', data)
+    return <div>Unsupported view data: {data['type']}</div>
 }
 
 export default View
